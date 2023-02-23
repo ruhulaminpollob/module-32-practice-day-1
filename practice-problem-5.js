a0e516c939d10020f986c9c69ad5a337901e24f8
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-23 20:40:15 +0600

    practice problem 5 complete

---
+++ practice-problem-5.js
@@ -0,0 +1,9 @@
+/*
+Q.
+৫. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
+
+*/
+
+const numbers=[3,4,6,8,2];
+const mapMultiply=numbers.map(number=>number*5);
+console.log(mapMultiply);
