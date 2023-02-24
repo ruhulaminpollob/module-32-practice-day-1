99e9834a926cfcba85e717e2dd2cf76bcfacbb0e
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-24 12:41:02 +0600

    practice problem 7.1 complete

---
+++ practice-problem-7.js
@@ -0,0 +1,17 @@
+/*
+৭. একটা array এর মধ্যে অনেকগুলা অবজেক্ট আছে। সেখানে যেই অবজেক্ট এর price আছে ৫০০০ টেক্কা সেই অবজেক্টকে find দিয়ে বের করো। 
+*/
+const gadgets=[
+    {name:'phone', price:10000},
+    {name:'router', price:5000},
+    {name:'laptop', price:180000},
+    {name:'watch', price:1000},
+];
+
+const which5k=gadgets.find(x=>x.price===5000);
+console.log(which5k);
+
+
+/*
+৭.৫ [এক্সট্রা] জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+*/
