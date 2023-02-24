fc5c162d3a1a5000fb1ef3f917aac169a3b4f1a5
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-24 21:28:07 +0600

    array destructuring practice problem solve

---
+++ practice-problem-9.js
@@ -0,0 +1,8 @@
+/*
+Q.
+৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+*/
+const arr=[23,43,64,755,344];
+
+const [one,two,three]=arr;
+console.log(three);
